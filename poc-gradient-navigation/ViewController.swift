c3a22fdbe95016465b6214bf2c9aa40f11ff784f
Ricardo Yamaguchi <ricardoy@ciandt.com>
2018-04-03 15:44:42 -0300

    Initial Commit

---
+++ poc-gradient-navigation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  poc-gradient-navigation
+//
+//  Created by Ricardo Yamaguchi on 03/04/18.
+//  Copyright © 2018 Ricardo Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
