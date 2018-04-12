f297135c2369002e3c3771ee53b78e4805f3b68b
Ricardo Yamaguchi <ricardoy@ciandt.com>
2018-04-12 15:48:10 -0300

    First commit

---
+++ poc-gradient-navigation/views/MainNavigationBar.swift
@@ -0,0 +1,21 @@
+//
+//  MainNavigationBar.swift
+//  poc-gradient-navigation
+//
+//  Created by Ricardo Yamaguchi on 12/04/18.
+//  Copyright © 2018 Ricardo Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationBar: UINavigationBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
