0b59d07ea273a16bb930a090c00e29403c14ce2a
Ricardo Yamaguchi <ricardoyamaguchi2011@gmail.com>
2018-04-16 06:58:38 -0300

    Remake

---
+++ poc-gradient-navigation/extensions/UIColor+Custom.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Custom.swift
+//  poc-gradient-navigation
+//
+//  Created by MacBook on 15/04/18.
+//  Copyright © 2018 Ricardo Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class UIColor_Custom: UIColor {
+
+}
