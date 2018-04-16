2d4c1f359a045a9c6ec75609ee7e107277c4cd31
Ricardo Yamaguchi <ricardoy@ciandt.com>
2018-04-16 11:08:36 -0300

    Another remake

--- poc-gradient-navigation/controllers/BaseViewController.swift
+++ poc-gradient-navigation/controllers/BaseViewController.swift
@@ -10,26 +10,44 @@ import UIKit
 
 class BaseViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var navigationBarColors: Array<UIColor> {
+        get {
+            return [UIColor.black, UIColor.lightGray]
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        applyGradient(to: (navigationController?.navigationBar)!)
     }
-    */
+    
+    func applyGradient(to navigationBar: UINavigationBar) {
+        let gradient = createGradientLayer(withColors: navigationBarColors, view:navigationBar)
+        navigationBar.setBackgroundImage(image(fromLayer: gradient), for: .default)
+        applyFontColor(with: navigationBarColors[0], navigation: navigationBar)
+    }
+    
+    func applyFontColor(with color: UIColor, navigation navigationBar: UINavigationBar) {
+        UIApplication.shared.statusBarStyle = color.isLight() ? .default : .lightContent
+        navigationBar.titleTextAttributes =  [NSAttributedStringKey.foregroundColor: color.isLight() ? UIColor.black : UIColor.white]
+    }
+    
+    func createGradientLayer(withColors colors: Array<UIColor>, view navigationBar: UINavigationBar) -> CAGradientLayer {
+        let gradient = CAGradientLayer()
+        gradient.frame = navigationBar.bounds
+        gradient.colors = [colors[0].cgColor, colors[1].cgColor]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 0)
+        return gradient
+    }
+    
+    func image(fromLayer layer: CAGradientLayer) -> UIImage {
+        UIGraphicsBeginImageContext(layer.bounds.size)
+        layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+    
 
 }
--- poc-gradient-navigation/extensions/UIColor+Custom.swift
+++ poc-gradient-navigation/extensions/UIColor+Custom.swift
@@ -8,6 +8,24 @@
 
 import UIKit
 
-class UIColor_Custom: UIColor {
+extension UIColor {
 
+    static var safetyOrange: UIColor {
+        get {
+            return UIColor(red: 230/255.0, green: 97/255.0, blue: 0/255.0, alpha: 1)
+        }
+    }
+
+    static var orangePeel: UIColor {
+        get {
+            return UIColor(red: 254/255.0, green: 161/255.0, blue: 0/255.0, alpha: 1)
+        }
+    }
+
+    func isLight() -> Bool {
+        guard let components = cgColor.components else {return false}
+        let brightness = ((components[0] * 299) + (components[1] * 587) + (components[2] * 114)) / 1000
+        return brightness > 0.5
+    }
+    
 }
